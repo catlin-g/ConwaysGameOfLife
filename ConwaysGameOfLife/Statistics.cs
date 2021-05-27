be0615838f5b456ed565a0ad498f5b4882b25ce6
Caitlin <caitlin.grange94@gmail.com>
2021-05-27 17:38:56 +1000

    added suggested changes to statistics clas

--- ConwaysGameOfLife/Statistics.cs
+++ ConwaysGameOfLife/Statistics.cs
@@ -8,14 +8,15 @@ namespace ConwaysGameOfLife
 		public int populationSize = 0;
 		public float percentAlive = 0f;
 		public int change = 0;
+		public const string fixConsolePrintIssue = "      ";
 
 		public void Print()
 		{
 			Console.WriteLine();
-			Console.WriteLine("Generation: " + generation + "   ");
-			Console.WriteLine("Population Size: " + populationSize + "   ");
-			Console.WriteLine("Percent Alive: " + percentAlive + "%   ");
-			Console.WriteLine("Change: " + change + "   ");
+			Console.WriteLine($"Generation: {generation}{fixConsolePrintIssue}");
+			Console.WriteLine($"Population Size: {populationSize}{fixConsolePrintIssue}");
+			Console.WriteLine($"Percent Alive: {percentAlive}%{fixConsolePrintIssue}");
+			Console.WriteLine($"Change: {change}{fixConsolePrintIssue}");
 		}
 	}
 }
